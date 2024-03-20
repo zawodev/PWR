63c26a66840400ffaf5f8590eff57b86b869e99a
zawo <alikstepaniuk@gmail.com>
2024-03-20 18:36:30 +0100

    fghjifaifhspbgsoifpsahn

--- sem4/JS-python/lab3/main.py
+++ sem4/JS-python/lab3/main.py
@@ -34,7 +34,7 @@ def read_log():
                     file_extension=fwe.split(".")[1] if "." in fwe else "",
 
                     response_code=int(parts[-2]),
-                    bytes_sent=int(parts[-1])
+                    bytes_sent=int(parts[-1]) if parts[-1].isdigit() else 0
                 ))
             except IndexError:
                 print(f"Line parsing Error! look in: {line}")
@@ -52,13 +52,16 @@ def sort_log(log, key):
 
 
 def get_entries_by_addr(log, addr):
+    entry: LogEntry
     filtered_entries = [entry for entry in log if entry.host_addr == addr and 100 <= entry.response_code < 600]
     return filtered_entries
 
 
 def get_entries_by_code(log, code):
+    entry: LogEntry
     filtered_entries = [entry for entry in log if entry.response_code == code and 100 <= entry.response_code < 600]
     return filtered_entries
+
 
 def get_failed_reads(log, merge=False):
     codes_4xx = []
@@ -75,53 +78,110 @@ def get_failed_reads(log, merge=False):
     else:
         return codes_4xx, codes_5xx
 
-def get_entries_by_extension():
-    pass
 
-def entry_to_dict():
-    pass
+def get_entries_by_extension(log, extension):
+    entry: LogEntry
+    filtered_entries = [entry for entry in log if entry.file_extension == extension]
+    return filtered_entries
 
-def log_to_dict():
-    pass
 
-def get_addrs():
-    pass
+def print_entries(entries):
+    for entry in entries:
+        print(entry.original_log, end="")
 
-def print_dict_entry_dates():
-    pass
 
-def test():
+def entry_to_dict(entry: LogEntry):
+    return {
+        "original_log": entry.original_log,
+        "host_addr": entry.host_addr,
+        "host_domain": entry.host_domain,
+        "date": entry.date,
+        "path": entry.path,
+        "file_extension": entry.file_extension,
+        "response_code": entry.response_code,
+        "bytes_sent": entry.bytes_sent
+    }
+
+
+def log_to_dict(log): # dict, w którym kluczem jest host_addr, a wartości to lista słowników z danymi
+    log_dict = {}
+    for entry in log:
+        if entry.host_addr in log_dict:
+            log_dict[entry.host_addr].append(entry_to_dict(entry))
+        else:
+            log_dict[entry.host_addr] = [entry_to_dict(entry)]
+    return log_dict
+
+
+def get_addrs(log_dict):
+    return log_dict.keys()
+
+
+def print_dict_entry_dates(log_dict):
+    for addr, entries in log_dict.items():
+        print(f"--- {addr} ---")
+        print(f"Number of requests: {len(entries)}")
+        print(f"First request date: {entries[0]['date']}")
+        print(f"Last request date: {entries[-1]['date']}")
+        print(f"200 response code ratio: {len([entry for entry in entries if entry['response_code'] == 200]) / len(entries)}")
+
+
+def test_all_func():
     # zad 1
     _log = read_log()
+    print("\n--- Original log: ---")
+    print_entries(_log)
 
     # zad 2
     _addr_to_search = "199.120.110.21"
-    for _entry in get_entries_by_addr(_log, _addr_to_search):
-        print(_entry.original_log, end="")
+    print(f"\n--- Entries for addr: {_addr_to_search} ---")
+    print_entries(get_entries_by_addr(_log, _addr_to_search))
 
     # zad 3
     _key = 3
-    for _entry in sort_log(_log, _key):
-        print(_entry[_key])
+    print(f"\n--- Sorted log by: {LogEntry._fields[_key]} ---")
+    print_entries(sort_log(_log, _key))
 
     # zad 4
     _code_to_search = 304
-    for _entry in get_entries_by_code(_log, _code_to_search):
-        print(_entry.original_log, end="")
+    print(f"\n--- Entries for code: {_code_to_search} ---")
+    print_entries(get_entries_by_code(_log, _code_to_search))
 
     # zad 5
     codes_4xx, codes_5xx = get_failed_reads(_log)
-    print("--- 4xx codes: ---")
-    for _entry in codes_4xx:
-        print(_entry.original_log, end="")
+    print("\n--- 4xx codes: ---")
+    print_entries(codes_4xx)
+
     print("--- 5xx codes: ---")
-    for _entry in codes_5xx:
-        print(_entry.original_log, end="")
+    print_entries(codes_5xx)
 
     merged_list = get_failed_reads(_log, merge=True)
-    print("--- merged list: ---")
-    for entry in merged_list:
-        print(entry.original_log, end="")
+    print("--- Merged list: ---")
+    print_entries(merged_list)
+
+    # zad 6
+    _extension_to_search = "gif"
+    print(f"\n--- Entries for extension: {_extension_to_search} ---")
+    print_entries(get_entries_by_extension(_log, _extension_to_search))
+
+    # zad 7
+    _log_dict = log_to_dict(_log)
+    print("\n--- Log dict: ---")
+    print(get_addrs(_log_dict))
+
+    # zad 8
+    print("\n--- Entries dates: ---")
+    print_dict_entry_dates(_log_dict)
+
+def test_last_func():
+    _log_dict = log_to_dict(get_entries_by_addr(read_log(), "ppp0.pwrnet.com"))
+    print("\n--- Log dict: ---")
+    print(get_addrs(_log_dict))
+
+    print("\n--- Entries dates: ---")
+    print_dict_entry_dates(_log_dict)
+
 
 if __name__ == "__main__":
-    test()
+    # test_all_func()
+    test_last_func()
