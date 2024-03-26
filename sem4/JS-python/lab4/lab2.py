9ae8c09b14356b384501d8ace0e472ba01a7eac4
zawo <alikstepaniuk@gmail.com>
2024-03-26 08:35:05 +0100

    bn

--- sem4/JS-python/lab4/lab2.py
+++ sem4/JS-python/lab4/lab2.py
@@ -2,26 +2,24 @@ import os
 import sys
 
 
-def wypisz_katalogi():
+def print_folders():
     for katalog in os.environ['PATH'].split(os.pathsep):
         print(katalog)
 
 
-def wypisz_pliki_wykonywalne():
-    for katalog in os.environ['PATH'].split(os.pathsep):
-        try:
-            # Listuje pliki w danym katalogu
-            pliki = os.listdir(katalog)
-        except OSError:
-            # Pomija katalog, jeśli wystąpiły błędy (np. brak dostępu)
+def print_exec_files():
+    for folder in os.environ['PATH'].split(os.pathsep):
+        try:  # listuje files w danym katalogu
+            files = os.listdir(folder)
+        except OSError:  # jeśli są błędy, pomijam katalog (np. brak dostępu)
             continue
 
-        wykonywalne_pliki = [plik for plik in pliki if os.path.isfile(os.path.join(katalog, plik)) and os.access(os.path.join(katalog, plik), os.X_OK)]
-        if wykonywalne_pliki:
-            print(f"{katalog}:")
-            for plik in wykonywalne_pliki:
+        exec_files = [plik for plik in files if os.path.isfile(os.path.join(folder, plik)) and os.access(os.path.join(folder, plik), os.X_OK)]
+        if exec_files:  # jeśli w danym folderze są pliki wykonywalne, to je wypisz
+            print(f"{folder}:")
+            for plik in exec_files:
                 print(f"  {plik}")
-            print()  # Dla lepszej czytelności
+            print()  # dla lepszej czytelności
 
 
 if __name__ == "__main__":
@@ -29,8 +27,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("No parameter given. " + usage_msg)
     elif sys.argv[1].lower() == "f":
-        wypisz_katalogi()
+        print_folders()
     elif sys.argv[1].lower() == "x":
-        wypisz_pliki_wykonywalne()
+        print_exec_files()
     else:
         print("Unknown parameter. " + usage_msg)
