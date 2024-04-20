90583fbb348ae82000584cd959c617d246308f6e
zawo <alikstepaniuk@gmail.com>
2024-04-21 01:14:31 +0200

    b

--- sem4/JS-python/lab5/main.py
+++ sem4/JS-python/lab5/main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
+import re
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# DEFINITIONS:
+# file - list of lines
+# line - raw line of text from file
+# log - parsed line of data in form of a dict
 
 
-# Press the green button in the gutter to run the script.
+def parse_log_file(file):
+    log_dicts = []
+    for line in file:
+        parsed_line = parse_log_line(line.strip())
+        if parsed_line:
+            log_dicts.append(parsed_line)
+    return log_dicts
+
+
+def parse_log_line(line: str): # 2a
+    """
+    Converts raw line of text into a data in a form of a dictionary
+    :param line: raw line (str)
+    :return: log in dictionary structure
+    """
+    # Dec 10 07:13:56 LabSZ sshd[24227]: Disconnecting: Too many authentication failures for root [preauth]
+    log_pattern = r'^(?P<timestamp>\w{3}\s+\d+\s+\d+:\d+:\d+)\s+(?P<hostname>\S+)\s+sshd\[(?P<pid>\d+)\]:\s+(?P<event>.+)$'
+    match = re.match(log_pattern, line)
+    if match:
+        return match.groupdict()
+    else:
+        return None
+
+
+def get_ipv4s_from_log(log): # 2b
+    ipv4_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    return re.findall(ipv4_pattern, log['event'])
+
+
+def get_user_from_log(log):
+    user_patterns = [
+        r'user\s+(\S+)',
+        r'user=(\S+)'
+    ]
+    for pattern in user_patterns:
+        match = re.search(pattern, log['event'])
+        if match:
+            return match.group(1)
+    return None
+
+
+def get_message_type(event_description):
+    event_description = event_description.lower()
+    phrases = ["accepted password for",
+               "failed password for",
+               "connection closed by",
+               "invalid user",
+               "authentication failure",
+               "possible break-in attempt"]
+    for phrase in phrases:
+        if phrase in event_description:
+            return phrase
+    return "other"
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    log_file = parse_log_file(sys.stdin)
+    for log_line in log_file:
+        print("=============")
+        print(get_ipv4s_from_log(log_line))
+        print(get_user_from_log(log_line))
+        print(get_message_type(log_line['event']))
+    print("=============")
