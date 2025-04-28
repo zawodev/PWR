285340682ed38bf5b7ddf13471f81e28fa9c0bac
zawodev <alikstepaniuk@gmail.com>
2025-04-29 01:12:09 +0200

    yy

--- sem6/AWS/lab2/py-proj/_ec2-ssh/db-test.py
+++ sem6/AWS/lab2/py-proj/_ec2-ssh/db-test.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 import os
 
-engine = create_engine("postgresql://zawodev:Gulasz123@booking-db-instance-1.chukay8wkkmn.us-east-1.rds.amazonaws.com:5432/ticketing")
+engine = create_engine("postgresql://zawodev:Haslo123@booking-db-instance-1.chukay8wkkmn.us-east-1.rds.amazonaws.com:5432/ticketing")
 conn = engine.connect()
 print("Connected:", conn.closed == False)
 conn.close()
